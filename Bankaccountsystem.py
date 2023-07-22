e66b86833df9d49a816eba9d70092dbc5d2337ac
Dray03 <139707967+Dray03@users.noreply.github.com>
2023-07-22 22:55:08 +0000

    Bank Account System
    This is a snippet code which contains a deposit instance method which allows you to deposit money into your account, withdraw instance method and balance instance method

---
+++ Bankaccountsystem.py
@@ -0,0 +1,27 @@
+# Bank Account System
+class BankAccount:
+    # Class Attribute
+    bank = "absa"
+
+    def __init__(self, name, money):
+        self.name = name
+        self.money = money
+        
+    def balance(self):
+            return f'You have ${self.money:,} left in your account'
+    
+    # Instance method Deposite
+    def deposite(self, cash):
+        self.money = self.money + cash
+        return f'You deposite ${self.money:,} to your account'
+
+    # Instance method Withdrawal
+    def withdraw(self, cash):
+        self.money = self.money - cash
+        if self.money < cash:
+            print("You have less money in your account")
+        else:
+            print(f"You withdrew ${self.money:,} from your account")
+
+    
+    
